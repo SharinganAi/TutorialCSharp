354fce07a5db7c81f81c28b84f9ec28efd662215
SharinganAi <dilipkr.18@gmail.com>
2018-10-28 19:23:39 +0530

    doing linq oeration on collections like List and array; Select; sort and selection from two differnet collectiona and then forming custom collection from the search result using Linq

--- linq1/Program.cs
+++ linq1/Program.cs
@@ -9,11 +9,24 @@ namespace linq1
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[]{1, 2, 3,  4, 5, 6 ,7, 8, 9, 10, 1, 11, 12};
-            oddNumbers(numbers);
+            var um = new UniversityManager();
+            
+            // um.MaleStudents();
+            // um.FemaleStudent();
+            um.SortStudentByAge();
+            System.Console.WriteLine("Select Univerity to see studentlist from it");
+            foreach(var uni in um.universities)
+            {
+                System.Console.WriteLine($"{uni.Id}. {uni.Name}");
+            }
+
+            System.Console.WriteLine("choose index of the university");
+            string index = Console.ReadLine();
+            um.AllStudentfromUniversity(index);
+            um.StudentAndUniversityNameCollection();
+
             Console.ReadKey();
         }
-
         static void oddNumbers(int[] num)
         {
             Console.WriteLine("Odd numbers:");
@@ -21,12 +34,10 @@ namespace linq1
             Console.WriteLine(string.Join(",", oddNumbers));
         }
     }
-
     class  University
     {
         public int Id { get; set; } 
         public string Name { get; set; }    
-
         public void Print()
         {
             Console.WriteLine(@"University {id} with {Name}");
@@ -35,6 +46,7 @@ namespace linq1
 
     class Student
     {
+
         public int Id{get; set;}
         public string Name { get; set; }
         public string Gender { get; set; }
@@ -47,7 +59,7 @@ namespace linq1
     }
 
     class UniversityManager{
-        List<University> universities = new List<University>();
+        public List<University> universities = new List<University>();
         List<Student> students = new List<Student>();
         public UniversityManager()
        {
@@ -58,17 +70,74 @@ namespace linq1
                 });
             students.AddRange(
                 new List<Student>(){
-                    new Student{Id = 1, Name = "Carla", Gender="Female", Age = 27, UniversityId= 1}
-                    new Student{Id = 1, Name = "Carla", Gender="Female", Age = 27, UniversityId= 1}
-                    new Student{Id = 1, Name = "Carla", Gender="Female", Age = 27, UniversityId= 1}
-                    new Student{Id = 1, Name = "Carla", Gender="Female", Age = 27, UniversityId= 1}
-                    new Student{Id = 1, Name = "Carla", Gender="Female", Age = 27, UniversityId= 1}
-                    new Student{Id = 1, Name = "Carla", Gender="Female", Age = 27, UniversityId= 1}
+                    new Student{Id = 1, Name = "Carla", Gender="Female", Age = 27, UniversityId= 1},
+                    new Student{Id = 2, Name = "Toni", Gender="male", Age = 21, UniversityId= 1},
+                    new Student{Id = 3, Name = "Frank", Gender="male", Age = 22, UniversityId= 1},
+                    new Student{Id = 4, Name = "Leyla", Gender="Female", Age = 19, UniversityId= 2},
+                    new Student{Id = 5, Name = "James", Gender="trans-gender", Age = 25, UniversityId= 2},
+                    new Student{Id = 6, Name = "Linda", Gender="Female", Age = 22, UniversityId= 2}
                 }
-            )
+            );
         }
 
+        public void MaleStudents()
+        {
+            var maleStudents = from student in students where student.Gender=="male" select student;
+            foreach(var student in maleStudents)
+            {
+                student.Print();
+            }
+        }
 
+        public void FemaleStudent()
+        {
+            var femaleStudents = from student in students where student.Gender=="Female" select student;
+            foreach(var student in femaleStudents)
+            {
+                student.Print();
+            }
+        }
+
+        public void SortStudentByAge()
+        {
+            var studentList = from student in students orderby student.Age select student;
+            foreach(var student in studentList)
+            {
+                student.Print();
+            }
+        }
+
+        public void AllStudentfromUniversity(string id)
+        {
+            var iitStudentList =  from student in students
+                                    where student.UniversityId.ToString() ==id
+                                    select student; 
+            if(iitStudentList.Count() >0)
+            {
+                foreach(var student in iitStudentList)
+                {
+                    student.Print();
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("No student present in the selected university");
+            }
+        }
+
+        //create a new Collection from two tables
+        public void StudentAndUniversityNameCollection()
+        {
+            var newCollection = from x in students
+                                join y in universities on x.UniversityId equals y.Id
+                                orderby x.Name
+                                select new {StudentName = x.Name, UniverityName= y.Name};
+            System.Console.WriteLine("The result is ");
+            foreach(var item in newCollection)
+            {
+                System.Console.WriteLine($"student name: {item.StudentName}, UinversityName: {item.UniverityName}");
+            }
+
+        }
     }
-
 }
